75fac187cc6ac4c44ea0e3a531a549ed68833c33
SmallCodeNote <132815163+SmallCodeNote@users.noreply.github.com>
2024-03-19 05:58:05 +0900

    Add files via upload

---
+++ SocketReceiverBase/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SocketReceiverBase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_StatusChange = new System.Windows.Forms.Button();
+            this.textBox_PortNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_LockedFrom = new System.Windows.Forms.Label();
+            this.label_LockedTime = new System.Windows.Forms.Label();
+            this.label_ResetTime = new System.Windows.Forms.Label();
+            this.label_RemainingTime = new System.Windows.Forms.Label();
+            this.button_Lock = new System.Windows.Forms.Button();
+            this.timer_LabelUpdate = new System.Windows.Forms.Timer(this.components);
+            this.timer_UpdateQueue = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button_StatusChange
+            // 
+            this.button_StatusChange.Location = new System.Drawing.Point(12, 8);
+            this.button_StatusChange.Name = "button_StatusChange";
+            this.button_StatusChange.Size = new System.Drawing.Size(41, 38);
+            this.button_StatusChange.TabIndex = 0;
+            this.button_StatusChange.UseVisualStyleBackColor = true;
+            this.button_StatusChange.EnabledChanged += new System.EventHandler(this.button_StatusChange_EnabledChanged);
+            this.button_StatusChange.Click += new System.EventHandler(this.button_StatusChange_Click);
+            // 
+            // textBox_PortNumber
+            // 
+            this.textBox_PortNumber.Location = new System.Drawing.Point(72, 27);
+            this.textBox_PortNumber.Name = "textBox_PortNumber";
+            this.textBox_PortNumber.Size = new System.Drawing.Size(63, 19);
+            this.textBox_PortNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "PortNumber";
+            // 
+            // label_LockedFrom
+            // 
+            this.label_LockedFrom.AutoSize = true;
+            this.label_LockedFrom.Location = new System.Drawing.Point(172, 9);
+            this.label_LockedFrom.Name = "label_LockedFrom";
+            this.label_LockedFrom.Size = new System.Drawing.Size(11, 12);
+            this.label_LockedFrom.TabIndex = 3;
+            this.label_LockedFrom.Text = "-";
+            // 
+            // label_LockedTime
+            // 
+            this.label_LockedTime.AutoSize = true;
+            this.label_LockedTime.Location = new System.Drawing.Point(172, 21);
+            this.label_LockedTime.Name = "label_LockedTime";
+            this.label_LockedTime.Size = new System.Drawing.Size(11, 12);
+            this.label_LockedTime.TabIndex = 3;
+            this.label_LockedTime.Text = "-";
+            // 
+            // label_ResetTime
+            // 
+            this.label_ResetTime.AutoSize = true;
+            this.label_ResetTime.Location = new System.Drawing.Point(172, 34);
+            this.label_ResetTime.Name = "label_ResetTime";
+            this.label_ResetTime.Size = new System.Drawing.Size(11, 12);
+            this.label_ResetTime.TabIndex = 3;
+            this.label_ResetTime.Text = "-";
+            // 
+            // label_RemainingTime
+            // 
+            this.label_RemainingTime.AutoSize = true;
+            this.label_RemainingTime.Location = new System.Drawing.Point(172, 46);
+            this.label_RemainingTime.Name = "label_RemainingTime";
+            this.label_RemainingTime.Size = new System.Drawing.Size(11, 12);
+            this.label_RemainingTime.TabIndex = 3;
+            this.label_RemainingTime.Text = "-";
+            // 
+            // button_Lock
+            // 
+            this.button_Lock.Location = new System.Drawing.Point(361, 9);
+            this.button_Lock.Name = "button_Lock";
+            this.button_Lock.Size = new System.Drawing.Size(26, 23);
+            this.button_Lock.TabIndex = 4;
+            this.button_Lock.UseVisualStyleBackColor = true;
+            // 
+            // timer_LabelUpdate
+            // 
+            this.timer_LabelUpdate.Interval = 1000;
+            this.timer_LabelUpdate.Tick += new System.EventHandler(this.timer_LabelUpdate_Tick_Tick);
+            // 
+            // timer_UpdateQueue
+            // 
+            this.timer_UpdateQueue.Interval = 1000;
+            this.timer_UpdateQueue.Tick += new System.EventHandler(this.timer_UpdateQueue_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 64);
+            this.Controls.Add(this.button_Lock);
+            this.Controls.Add(this.label_RemainingTime);
+            this.Controls.Add(this.label_ResetTime);
+            this.Controls.Add(this.label_LockedTime);
+            this.Controls.Add(this.label_LockedFrom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_PortNumber);
+            this.Controls.Add(this.button_StatusChange);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "SocketReceiverBase";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_StatusChange;
+        private System.Windows.Forms.TextBox textBox_PortNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_LockedFrom;
+        private System.Windows.Forms.Label label_LockedTime;
+        private System.Windows.Forms.Label label_ResetTime;
+        private System.Windows.Forms.Label label_RemainingTime;
+        private System.Windows.Forms.Button button_Lock;
+        private System.Windows.Forms.Timer timer_LabelUpdate;
+        private System.Windows.Forms.Timer timer_UpdateQueue;
+    }
+}
+
---
+++ SocketReceiverBase/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WinFormStringCnvClass;
+
+using tcpServer;
+using tcpClient;
+
+namespace SocketReceiverBase
+{
+    public partial class Form1 : Form
+    {
+        //===================
+        // Constructor
+        //===================
+        public Form1()
+        {
+            InitializeComponent();
+            thisExeDirPath = Path.GetDirectoryName(Application.ExecutablePath);
+
+            tcpSrv = new TcpSocketServer();
+            tcpClt = new TcpSocketClient();
+
+            LockReleaseTime = DateTime.Now;
+
+        }
+
+        //===================
+        // Member variable
+        //===================
+        TcpSocketServer tcpSrv;
+        TcpSocketClient tcpClt;
+
+        string thisExeDirPath;
+
+        string RunMode = "Free";//Free,Lock
+        string StatusMode = "OK";//OK,NG,Error
+        public string LockSignalSourceName = "";
+
+        public DateTime LockReleaseTime;
+        public DateTime LockTime;
+
+        DateTime LastCheckTime;
+
+        public bool Locked { get { return (DateTime.Now - LockReleaseTime).TotalSeconds < 0; } }
+
+        public bool Error
+        {
+            get
+            {
+                return button_StatusChange.BackColor == Color.Red;
+            }
+            set
+            {
+                if (value)
+                {
+                    button_StatusChange.BackColor = Color.Red;
+                    StatusMode = "NG";
+                }
+                else
+                {
+                    button_StatusChange.BackColor = Color.YellowGreen;
+                    StatusMode = "OK";
+                }
+
+                tcpSrv.ResponceMessage = RunMode + "\t" + StatusMode;
+            }
+        }
+
+        public int PortNumberSrv
+        {
+            get
+            {
+                int b = -1;
+                if (!int.TryParse(textBox_PortNumber.Text, out b)) { b = -1; }
+                return b;
+            }
+            set
+            {
+                textBox_PortNumber.Text = value.ToString();
+            }
+        }
+
+        //===================
+        // Member function
+        //===================
+
+        public void LockClient(int Minutes)
+        {
+            LockTime = DateTime.Now;
+            LockReleaseTime = DateTime.Now + new TimeSpan(0, Minutes, 0);
+        }
+
+
+        private void LabelUpdate()
+        {
+
+            if ((DateTime.Now - LockReleaseTime).TotalSeconds > 0)
+            {
+                label_LockedFrom.Text = "- Release -";
+                label_LockedTime.Text = "- Release -";
+                label_ResetTime.Text = "- Release -";
+                button_Lock.Text = "";
+                button_Lock.BackColor = Color.YellowGreen;
+            }
+            else
+            {
+                label_LockedFrom.Text = LockSignalSourceName;
+                label_LockedTime.Text = LockTime.ToString("yyyy/MM/dd HH:mm:ss");
+                label_ResetTime.Text = LockReleaseTime.ToString("yyyy/MM/dd HH:mm:ss");
+                label_RemainingTime.Text = getElapsedTimeString(LockReleaseTime - DateTime.Now);
+
+                button_Lock.Text = "";
+                button_Lock.BackColor = Color.Gray;
+            }
+
+        }
+
+        public string getElapsedTimeString(TimeSpan elapsedTime)
+        {
+            if (elapsedTime.TotalDays >= 365) { return (elapsedTime.TotalDays / 365.2425).ToString("0") + " year"; }
+            if (elapsedTime.TotalDays >= 30) { return (elapsedTime.TotalDays / 30.436875).ToString("0") + " month"; }
+            if (elapsedTime.TotalDays >= 7) { return (elapsedTime.TotalDays / 7).ToString("0") + " week"; }
+            if (elapsedTime.TotalDays >= 1) { return (elapsedTime.TotalDays / 7).ToString("0") + " day"; }
+            if (elapsedTime.TotalHours >= 1) { return (elapsedTime.TotalHours).ToString("0") + " hour"; }
+            if (elapsedTime.TotalMinutes >= 1) { return (elapsedTime.TotalMinutes).ToString("0") + " minute"; }
+            if (elapsedTime.TotalSeconds >= 1) { return (elapsedTime.TotalSeconds).ToString("0") + " sec."; }
+            return "now";
+        }
+
+        //===================
+        // Event
+        //===================
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "TEXT|*.txt";
+            if (false && ofd.ShowDialog() == DialogResult.OK)
+            {
+                WinFormStringCnv.setControlFromString(this, File.ReadAllText(ofd.FileName));
+            }
+            else
+            {
+                string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+                if (File.Exists(paramFilename))
+                {
+                    WinFormStringCnv.setControlFromString(this, File.ReadAllText(paramFilename));
+                }
+            }
+            timer_UpdateQueue.Start();
+            timer_LabelUpdate.Start();
+            Task<bool> srvTask = tcpSrv.StartListening(PortNumberSrv, "UTF8");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string FormContents = WinFormStringCnv.ToString(this);
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "TEXT|*.txt";
+
+            if (false && sfd.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(sfd.FileName, FormContents);
+            }
+            else
+            {
+                string paramFilename = Path.Combine(thisExeDirPath, "_param.txt");
+                File.WriteAllText(paramFilename, FormContents);
+            }
+        }
+
+        private void button_StatusChange_Click(object sender, EventArgs e)
+        {
+            Error = !Error;
+        }
+
+        private void button_StatusChange_EnabledChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer_LabelUpdate_Tick_Tick(object sender, EventArgs e)
+        {
+            LabelUpdate();
+        }
+
+        private void timer_UpdateQueue_Tick(object sender, EventArgs e)
+        {
+            timer_UpdateQueue.Stop();
+            //============
+            // New data check from Queue
+            //============
+            if ((tcpSrv.LastReceiveTime - LastCheckTime).TotalSeconds > 0 && tcpSrv.ReceivedSocketQueue.Count > 0)
+            {
+                string receivedSocketMessage = "";
+
+                //============
+                // ReadQueue 
+                //============
+                while (tcpSrv.ReceivedSocketQueue.TryDequeue(out receivedSocketMessage))
+                {
+
+                    if (receivedSocketMessage.IndexOf("Lock") >= 0)
+                    {
+                        string[] cols = receivedSocketMessage.Split('\t');
+
+                        if (cols.Length > 2 && int.TryParse(cols[2], out int Minutes))
+                        {
+                            LockTime = DateTime.Now;
+                            LockReleaseTime = DateTime.Now + new TimeSpan(0, Minutes, 0);
+                        }
+                    }
+                    else if (receivedSocketMessage.IndexOf("Release") >= 0)
+                    {
+                        LockReleaseTime = DateTime.Now;
+
+                    }
+
+                }
+                LastCheckTime = DateTime.Now;
+
+            }
+
+            timer_UpdateQueue.Start();
+
+        }
+    }
+}
